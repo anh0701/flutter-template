54dd576d2d11bbf122a82b84f9626aed9676e69c
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-01-25 12:23:47 +0700

    add textfeild phone number

--- lib/custom_widget/input.dart
+++ lib/custom_widget/input.dart
@@ -5,77 +5,105 @@ Widget customTextField(
     {required TextInputType textInputType,
     String? hintText,
     InputBorder? border,
-    Widget? suffixIcon, TextEditingController? controller}) {
+    Widget? suffixIcon,
+    bool obscureText = true,
+    Color? iconColor = Colors.blue,
+    InputBorder? errorBorder,
+    TextEditingController? controller}) {
   RegExp patternEmail = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-  RegExp patternPassword = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
-  RegExp patternPhone = RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$');
+  RegExp patternPassword =
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+  RegExp patternPhone = RegExp(r'(^(\+[0-9]{1,3}[- ]?)?\d{10,12}$)');
+
   String error = "";
+
   if (textInputType == TextInputType.visiblePassword) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
+          iconColor: iconColor,
           hintStyle: TextStyle(fontSize: 17),
           errorText: error,
-          errorStyle: TextStyle(
-            color: error == "Correct." ? Colors.green : Colors.red,
-          ),
           hintText: 'Password',
           border: border,
           suffixIcon: suffixIcon,
-          contentPadding: EdgeInsets.all(20)),
+          contentPadding: EdgeInsets.all(20),
+          errorBorder: errorBorder),
       keyboardType: TextInputType.visiblePassword,
-       autovalidateMode: AutovalidateMode.onUserInteraction,
-      obscureText: true,
-      validator: (value){
-        if(value!.isEmpty){
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      obscureText: obscureText,
+      validator: (value) {
+        if (value!.isEmpty) {
           error = "Please input your password!";
-        } else if(patternPassword.hasMatch(value)){
-          error = "Correct.";
-        }
-        else if(!patternPassword.hasMatch(value)){
-          error = "Your password must be at 8-16 characters including a lowercase letter, an uppercase letter, and a number.";
+        } else if (patternPassword.hasMatch(value)) {
+          error = "";
+        } else if (!patternPassword.hasMatch(value)) {
+          error =
+              "Your password must be at 8-16 characters including a lowercase letter, an uppercase letter, and a number.";
         }
         return error;
       },
     );
-  } else if(textInputType == TextInputType.emailAddress) {
+  } else if (textInputType == TextInputType.emailAddress) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
           hintStyle: TextStyle(fontSize: 17),
           errorText: error,
-          errorStyle: TextStyle(
-            color: error == "Correct." ? Colors.green : Colors.red,
-          ),
           hintText: hintText,
           border: border,
+          errorBorder: errorBorder,
           suffixIcon: suffixIcon,
           contentPadding: EdgeInsets.all(20)),
       keyboardType: TextInputType.emailAddress,
-       autovalidateMode: AutovalidateMode.onUserInteraction,
-      validator: (value){
-        if(value!.isEmpty){
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) {
+        if (value!.isEmpty) {
           error = 'Please input your E-mail!';
-        }
-        else if( patternEmail.hasMatch(value) ){
-          error = 'Correct.';
-        }
-        else if( !patternEmail.hasMatch(value) ){
+        } else if (patternEmail.hasMatch(value)) {
+          error = '';
+        } else if (!patternEmail.hasMatch(value)) {
           error = 'Email is invalid.';
         }
         return error;
       },
     );
-  } else{
+  } else if(textInputType == TextInputType.phone) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
           hintStyle: TextStyle(fontSize: 17),
           hintText: hintText,
           border: border,
+          errorText: error,
+          errorBorder: errorBorder,
+          suffixIcon: suffixIcon,
+          contentPadding: EdgeInsets.all(20)),
+      keyboardType: TextInputType.phone,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) {
+        if (value!.isEmpty) {
+          error = 'Please input your Phone!';
+        } else if (patternPhone.hasMatch(value)) {
+          error = '';
+        } else if (!patternPhone.hasMatch(value)) {
+          error = 'Phone is invalid.';
+        }
+        return error;
+      },
+    );
+  }
+   else {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+          hintStyle: TextStyle(fontSize: 17),
+          hintText: hintText,
+          border: border,
+          errorBorder: errorBorder,
           suffixIcon: suffixIcon,
           contentPadding: EdgeInsets.all(20)),
       keyboardType: textInputType,
-    ); 
+    );
   }
 }
--- lib/views/message_page.dart
+++ lib/views/message_page.dart
@@ -13,41 +13,64 @@ class MessagePage extends StatefulWidget {
 }
 
 class _MessagePageState extends State<MessagePage> {
+  // String err = "correct";
+  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: [
           message_theme(
-          text_received: Text(
-            'Hi',
-            style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 13,
-                color: CustomColor.textCommonColor),
-          ),
-          text_sended:  Text(
-            'hello',
-            style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 13,
-                color: Colors.white),
-          ),
-          box_color_received: Color.fromARGB(31, 227, 20, 20),
-          box_color_sended: Color.fromARGB(255, 144, 89, 6)),
+              text_received: Text(
+                'Hi',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 13,
+                    color: CustomColor.textCommonColor),
+              ),
+              text_sended: Text(
+                'hello',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 13,
+                    color: Colors.white),
+              ),
+              box_color_received: Color.fromARGB(31, 227, 20, 20),
+              box_color_sended: Color.fromARGB(255, 144, 89, 6)),
           message_theme(
-          text_received: Text(
-            'where are you?',
-            style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 13,
-                color: CustomColor.textCommonColor),
+              text_received: Text(
+                'where are you?',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 13,
+                    color: CustomColor.textCommonColor),
+              ),
+              box_color_received: Color.fromARGB(31, 227, 20, 20)),
+
+          // input_error(),
+          customTextField(
+              textInputType: TextInputType.emailAddress,
+              hintText: 'enter email address'),
+          customTextField(
+              textInputType: TextInputType.phone,
+              hintText: 'enter phone number'),
+          customTextField(
+            textInputType: TextInputType.visiblePassword,
+            hintText: 'enter password',
+            obscureText: _isObscure,
+            suffixIcon: IconButton(
+              icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
+              onPressed: () {
+                setState(() {
+                  _isObscure = !_isObscure;
+                });
+              },
+            ),
           ),
-          box_color_received: Color.fromARGB(31, 227, 20, 20)), 
-      
-          // input_error(),  
-          customTextField(textInputType: TextInputType.emailAddress, hintText: 'enter email address'),   
-          customTextField(textInputType: TextInputType.visiblePassword, hintText: 'enter password'),   
+
+          // Text("hihi", style: TextStyle(
+          //   color: err == "correct" ? Colors.blue : Colors.red
+          // ),)
         ],
       ),
     );
